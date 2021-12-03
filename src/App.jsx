2ae631b687516875f2a073e27e06564543c747c4
BrandonMCarlson <Brandoncarlson93bc@gmail.com>
2021-12-02 19:18:48 -0800

    add new component files as well as axios in app.js. worked on with Dan, Cody, Peter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getAllUsers = async () => {
+  await axios.get('http:localhost:5000/api').then((response) => {
+    console.log(response.data);
+    setUsers(response.data);
+  });
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+ 
   return (
     <div className="App">
       <header className="App-header">
